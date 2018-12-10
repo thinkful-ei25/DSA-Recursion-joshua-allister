a7ef18a0d9610cf1f2dabae3dd150fe1fa4c8c5d
Allister Rooke <allisterg@gmail.com>
2018-12-10 14:46:57 -0500

    fibonacci

--- recursion.js
+++ recursion.js
@@ -71,11 +71,22 @@
 
 // console.log(binaryRep(100))
 
-function factorial(num){
-  if(num === 0){
-    return 1;
-  }
-  return num * factorial(num-1);
-}
+// function factorial(num){
+//   if(num === 0){
+//     return 1;
+//   }
+//   return num * factorial(num-1);
+// }
 
-console.log(factorial(2));
+// console.log(factorial(2));
+
+// function fibonacci(num) {
+//   if (num <= 2) {
+//     return 1
+//   }
+  
+//   return fibonacci(num-2) + fibonacci(num-1);
+
+// }
+
+// console.log(fibonacci(7))
