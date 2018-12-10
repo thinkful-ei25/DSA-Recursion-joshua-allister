a2b217f24af30ee3ae244c204512f4992aefb008
Joshua Adams <joshadams554@gmail.com>
2018-12-10 11:56:59 -0500

    reverse string

--- recursion.js
+++ recursion.js
@@ -23,3 +23,12 @@ function arrayDouble(arr) {
 const arr1 = [1, 2, 3]
 
 console.log(arrayDouble(arr1))
+
+function reverse(str){
+  if(str.length === 0){
+    return '';
+  }
+  return (str[str.length -1] + reverse(str.slice(0,str.length-1)));
+}
+
+console.log(reverse('reverse'));
